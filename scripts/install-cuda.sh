1efc37f3598bbc137726a97a01ade6a112dd1ceb
Kiran Ranganath <kiran7193@gmail.com>
2017-06-29 16:47:03 -0700

    Add vector-addition

--- scripts/install-cuda.sh
+++ scripts/install-cuda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-travis_retry wget https://developer.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64-deb
+travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 travis_retry sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 travis_retry sudo apt-get update -qq
 travis_retry sudo apt-get install -y cuda
